1b04083290cd59d38c69840ab7549d6146897c63
Won <trilliwon@gmail.com>
2020-01-01 20:43:22 +0900

    chore: Add https://leetcode.com/problems/combination-sum-iv/ v2

--- LeetCode/medium/combination-sum-iv.py
+++ LeetCode/medium/combination-sum-iv.py
@@ -3,20 +3,11 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [-1] * (target + 1)
+        dp = [0] * (target + 1)
+        dp[0] = 1
 
-        def combination(nums, target):
-            if dp[target] != -1:
-                return dp[target]
-            if target == 0:
-                return 1
-            result = 0
-            for n in nums:
-                if target - n >= 0:
-                    v = combination(nums, target - n)
-                    result += v
-            dp[target] = result
-            return result
-
-        answer = combination(nums, target)
-        return answer
+        for i in range(1, target + 1):
+            for j in range(0, len(nums)):
+                if (i - nums[j]) >= 0:
+                    dp[i] += dp[i - nums[j]]
+        return dp[target]
