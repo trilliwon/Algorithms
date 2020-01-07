304f2b4963198a22504c689acf5cb96a954867c9
Won <trilliwon@gmail.com>
2020-01-07 20:25:36 +0900

    Change medium - https://leetcode.com/problems/coin-change

--- LeetCode/medium/coin-change.py
+++ LeetCode/medium/coin-change.py
@@ -1,15 +1,21 @@
+from typing import List
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         if n == 0:
             return 0
 
-        coins.sort()
         dp = [-1 for _ in range(amount + 1)]
         dp[0] = 0
 
         for coin in coins:
-            for j in range(0, amount + 1):
-                if j >= coin and dp[j-coin] != -1:
-                    dp[j] = (1 + dp[j-coin]) if (dp[j] == -1) else min(dp[j], 1 + dp[j-coin])
-        return  dp[-1]
+            for i in range(1, amount + 1):
+                if coin <= i and dp[i-coin] != -1:
+                    if dp[i] == -1:
+                        dp[i] = 1 + dp[i-coin]
+                    else:
+                        dp[i] = min(dp[i], 1 + dp[i-coin])
+
+        return dp[-1]
